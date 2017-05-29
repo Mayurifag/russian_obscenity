fcf666114372468729ddf424bdc27deb984bda80
ivan <ivan.rabotyaga@gmail.com>
2017-05-29 21:00:58 +0300

    added white list

--- lib/russian_obscenity/base.rb
+++ lib/russian_obscenity/base.rb
@@ -4,15 +4,17 @@ require 'yaml'
 module RussianObscenity extend self
 
   DEFAULT_DICTIONARY = File.dirname(__FILE__) + "/../../config/dictionary.yml"
+  DEFAULT_WHITELIST = File.dirname(__FILE__) + "/../../config/whitelist.yml"
 
   @dictionary_list = [:default]
+  @whitelist = [:default]
 
   def obscene?(text)
     (text =~ regexp) ? true : false
   end
 
   def sanitize(text, replace = '*')
-    text.gsub(regexp, replace)
+    text.gsub(regexp) { |match| whitelisted_words.include?(match.mb_chars.downcase.to_s) ? match : replace }
   end
 
   def find(text)
@@ -29,6 +31,16 @@ module RussianObscenity extend self
     reload_regexp
   end
 
+  def whitelist
+    @whitelist
+  end
+
+  def whitelist=(list)
+    list = [:default] if list.nil? or list.empty?
+    @whitelist = list.uniq.compact
+    reload_whitelist
+  end
+
   private
 
   def regexp
@@ -36,8 +48,21 @@ module RussianObscenity extend self
     @regexp
   end
 
+  def whitelisted_words
+    reload_whitelist if @whitelisted_words.nil?
+    @whitelisted_words
+  end
+
   def reload_regexp
     @regexp = make_regexp(@dictionary_list)
+  end
+
+  def reload_whitelist
+    @whitelisted_words = []
+    @whitelist.each do |file|
+      file = DEFAULT_WHITELIST if file == :default
+      @whitelisted_words << YAML.load_file(file)
+    end
   end
 
   def make_regexp(dict_list)
